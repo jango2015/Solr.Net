3910ca6cbe2e04a2b6fcdca6f4f88eed544d197e
Theis Mackeprang Iversen <tmi@immeo.dk>
2015-07-17 16:22:22 +0200

    Improved error handling

--- Solr.Client/Solr.Client/DefaultSolrConfiguration.cs
+++ Solr.Client/Solr.Client/DefaultSolrConfiguration.cs
@@ -2,11 +2,13 @@
 using System.Collections.Specialized;
 using System.Configuration;
 using System.Linq;
+using log4net;
 
 namespace Solr.Client
 {
     public class DefaultSolrConfiguration : ISolrConfiguration
     {
+        private ILog _logger;
         private const string SettingCollectionUrl = "solr:collectionUrl";
         private const string SettingCollectionUpdateUrl = "solr:collectionUpdateUrl";
         private const string SettingCollectionQueryUrl = "solr:collectionQueryUrl";
@@ -14,33 +16,45 @@ namespace Solr.Client
         public DefaultSolrConfiguration(string collectionUrl)
         {
             var appSettings = ConfigurationManager.AppSettings;
-            SetDefaultRequestUrls(appSettings, collectionUrl);
+            Init(appSettings, collectionUrl);
         }
 
         public DefaultSolrConfiguration()
         {
             var appSettings = ConfigurationManager.AppSettings;
             var collectionUrl = GetValueFromAppSettings(appSettings, SettingCollectionUrl, null);
-            SetDefaultRequestUrls(appSettings, collectionUrl);
+            Init(appSettings, collectionUrl);
         }
 
-        private void SetDefaultRequestUrls(NameValueCollection appSettings, string collectionUrl)
+        private void Init(NameValueCollection appSettings, string collectionUrl)
         {
-            collectionUrl = string.IsNullOrWhiteSpace(collectionUrl) ? null : collectionUrl.TrimEnd('/');
-            var defaultUpdateUrl = collectionUrl == null ? null : string.Format("{0}/update", collectionUrl);
-            var defaultQueryUrl = collectionUrl == null ? null : string.Format("{0}/query", collectionUrl);
-            UpdateUrl = GetValueFromAppSettings(appSettings, SettingCollectionUpdateUrl, defaultUpdateUrl);
-            QueryUrl = GetValueFromAppSettings(appSettings, SettingCollectionQueryUrl, defaultQueryUrl);
-            if (UpdateUrl == null || QueryUrl == null)
+            _logger = LogManager.GetLogger(GetType());
+            try
             {
-                throw new TypeInitializationException(GetType().FullName,
-                    new ArgumentNullException(SettingCollectionUpdateUrl,
-                        string.Format("The app setting {0} should be configured to the base url of a solr core/collection, e.g.: http://localhost:8983/solr/collectionName, which supports both the /update and /query request handlers. Alternatively, explicit urls can be set for query and update handlers with the app settings {1} and {2}, respectively", SettingCollectionUrl, SettingCollectionQueryUrl, SettingCollectionUpdateUrl)));
+                collectionUrl = string.IsNullOrWhiteSpace(collectionUrl) ? null : collectionUrl.TrimEnd('/');
+                var defaultUpdateUrl = collectionUrl == null ? null : string.Format("{0}/update", collectionUrl);
+                var defaultQueryUrl = collectionUrl == null ? null : string.Format("{0}/query", collectionUrl);
+                UpdateUrl = GetValueFromAppSettings(appSettings, SettingCollectionUpdateUrl, defaultUpdateUrl);
+                QueryUrl = GetValueFromAppSettings(appSettings, SettingCollectionQueryUrl, defaultQueryUrl);
+                if (UpdateUrl == null || QueryUrl == null)
+                {
+                    throw new TypeInitializationException(GetType().FullName,
+                        new ArgumentNullException(SettingCollectionUpdateUrl,
+                            string.Format(
+                                "The app setting {0} should be configured to the base url of a solr core/collection, e.g.: http://localhost:8983/solr/collectionName, which supports both the /update and /query request handlers. Alternatively, explicit urls can be set for query and update handlers with the app settings {1} and {2}, respectively",
+                                SettingCollectionUrl, SettingCollectionQueryUrl, SettingCollectionUpdateUrl)));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Could not initialize solr configuration", ex);
+                throw;
             }
         }
 
         private static string GetValueFromAppSettings(NameValueCollection appSettings, string key, string defaultValue)
         {
+            if (appSettings == null) return defaultValue;
             var values = appSettings.GetValues(key);
             if (values == null) return defaultValue;
             return values.FirstOrDefault() ?? defaultValue;
